98b26823ce821f9985fc54484ff6d04381272829
John Shen <johnzhihaoshen@gmail.com>
2017-05-16 13:49:37 -0700

    added basic queue implementation and tests

---
+++ js/stack_queue.js
@@ -0,0 +1,81 @@
+class Node {
+    constructor(value, next=null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+export class Stack {
+    constructor() {
+        this.top = null;
+    }
+
+    pop() {
+        const topNode = this.top;
+        this.top = this.top.next;
+        return topNode;
+    }
+
+    push(node) {
+        node.next = this.top;
+        this.top = node;
+    }
+
+    pushValue(value) {
+        const node = new Node(value, this.top)
+        this.top = node;
+    }
+
+    peek() {
+        if (this.isEmpty()) {
+            throw new Error('Stack is empty');
+        }
+        return this.top;
+    }
+
+    isEmpty() {
+        return this.top === null;
+    }
+}
+
+export class Queue {
+    constructor() {
+        this.next = null;
+    }
+
+    add(node) {
+        let tail;
+        if (this.next === null) {
+            tail = this;
+        } else {
+            let currentNode = this.next;
+            while (currentNode.next !== null) {
+                currentNode = currentNode.next;
+            }
+            tail = currentNode;
+        }
+        tail.next = node;
+    }
+
+    addValue(value) {
+        const node = new Node(value);
+        this.add(node);
+    }
+
+    shift() {
+        const nodeToRemove = this.next;
+        this.next = this.next.next;
+        return nodeToRemove;
+    }
+
+    peek() {
+        if (this.isEmpty()) {
+            throw new Error('Queue is empty');
+        }
+        return this.next;
+    }
+
+    isEmpty() {
+        return this.next === null;
+    }
+}
---
+++ js/stack_queue.test.js
@@ -0,0 +1,58 @@
+import { Stack, Queue } from './stack_queue';
+
+describe('Stack', () => {
+    let stack;
+    beforeEach(() => {
+        stack = new Stack();
+        stack.pushValue('stack it');
+    })
+    test('creates a stack', () => {
+        expect(stack.top.value).toBe('stack it');
+    });
+    test('peek() returns the top node', () => {
+        expect(stack.peek().value).toBe('stack it');
+    });
+    test('pushValue(value) adds a node to the top of the stack', () => {
+        stack.pushValue(3);
+        expect(stack.peek().value).toBe(3);
+    });
+    test('isEmpty() returns the correct boolean', () => {
+        expect(stack.isEmpty()).toBe(false);
+        const emptyStack = new Stack();
+        expect(emptyStack.isEmpty()).toBe(true);
+    });
+    test('pop() removes and returns the top node', () => {
+        stack.pushValue(42);
+        expect(stack.pop().value).toBe(42);
+        expect(stack.pop().value).toBe('stack it');
+    });
+});
+
+
+describe('Queue', () => {
+    let queue;
+    beforeEach(() => {
+        queue = new Queue();
+        queue.addValue('queue it');
+    })
+    test('creates a queue', () => {
+        expect(queue.next.value).toBe('queue it');
+    });
+    test('peek() returns the first node', () => {
+        expect(queue.peek().value).toBe('queue it');
+    });
+    test('addValue(value) adds a node to the end of the queue', () => {
+        queue.addValue(3);
+        expect(queue.peek().value).toBe('queue it');
+    });
+    test('isEmpty() returns the correct boolean', () => {
+        expect(queue.isEmpty()).toBe(false);
+        const emptyQueue = new Queue();
+        expect(emptyQueue.isEmpty()).toBe(true);
+    });
+    test('shift() removes and returns the first node', () => {
+        queue.addValue(42);
+        expect(queue.shift().value).toBe('queue it');
+        expect(queue.shift().value).toBe(42);
+    });
+});
