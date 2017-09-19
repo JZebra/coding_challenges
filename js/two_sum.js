2989193642fb6aa748c06f40fb812473c9db5774
John Shen <johnzhihaoshen@gmail.com>
2017-09-18 19:35:30 -0700

    faster two_sum solution

--- js/two_sum.js
+++ js/two_sum.js
@@ -1,4 +1,4 @@
-const twoSum = (arr, target) => {
+const twoSumNaive = (arr, target) => {
     // generate an array of opposite pairs
     const diffs = arr.map(el => target - el)
 
@@ -11,4 +11,19 @@ const twoSum = (arr, target) => {
     }
 }
 
+// faster solution with hashmap and one walk
+const twoSum = (arr, target) => {
+    let elMap = new Map()
+
+    for (var i = 0; i < arr.length; i++) {
+        let val = arr[i];
+        let pairedIdx = elMap.get(target - val)
+        if (pairedIdx !== undefined && pairedIdx !== i) {
+            return [pairedIdx, i]
+        }
+        elMap.set(val, i)
+    }
+}
+
 console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([3, 3], 6))
