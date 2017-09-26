0608b78f31f38b277a84a5aababa072c032bc58b
John Shen <johnzhihaoshen@gmail.com>
2017-09-26 00:31:12 -0700

    fixed linter errors, changed js tab_size to 2

--- js/even_tree.js
+++ js/even_tree.js
@@ -1,65 +1,64 @@
 // https://www.hackerrank.com/challenges/even-tree
-const evenTree = input => {
-    // create a tree and reference the root
-    const rootNode = new Node('1', [])
-    const tree = parseInputAndGenerateTree(input, rootNode)
-    // dfs through the tree, assign a point value to each node
-    let stack = [tree];
-    let count = 0
-    // every node with an even count can be cut from its parent
-    while (stack.length > 0) {
-        let current = stack.pop()
-        stack = stack.concat(current.children)
-        // we can break the conditional into a hasSubtree() function on Node
-        if (current.getSize() % 2 === 0) {
-            count++
-        }
-    }
-    // reduce the count by one for the rootNode, which is always even
-    console.log(count - 1)
-    return count - 1
-}
-
 const parseInputAndGenerateTree = (input, rootNode) => {
-    input.split('\n').slice(1).forEach((str) => {
-        let [childValue, parentValue] = str.split(' ')
-        // this method traverses the whole tree and looks for a node with value === parentValue
-        let parentNode = rootNode.getNode(parentValue)
-        parentNode.addChild(childValue)
-    })
-    return rootNode
-}
+  input.split('\n').slice(1).forEach((str) => {
+    const [childValue, parentValue] = str.split(' ');
+    // this method traverses the whole tree and looks for a node with value === parentValue
+    const parentNode = rootNode.getNode(parentValue);
+    parentNode.addChild(childValue);
+  });
+  return rootNode;
+};
 
 class Node {
-    constructor(value, children) {
-        this.value = value;
-        this.children = children;
+  constructor(value, children) {
+    this.value = value;
+    this.children = children;
+  }
+  // DFS through children and return a node with the value param
+  getNode(value) {
+    let stack = [this];
+    const seenValues = {};
+    while (stack.length > 0) {
+      const current = stack.pop();
+      stack = stack.concat(current.children);
+      if (current.value === value) {
+        return current;
+      }
+      seenValues[current.value] = true;
     }
-    // DFS through children and return a node with the value param
-    getNode(value) {
-        let stack = [this]
-        let seenValues = {}
-        while (stack.length > 0) {
-            let current = stack.pop()
-            stack = stack.concat(current.children)
-            if (current.value == value) {
-                return current
-            } else {
-                seenValues[current.value] = true
-            }
-        }
-        return null
-    }
+    return null;
+  }
 
-    addChild(value) {
-        this.children.push(new Node(value, []));
-    }
+  addChild(value) {
+    this.children.push(new Node(value, []));
+  }
 
-    getSize() {
-        return this.children.reduce((acc, child) => { return acc + child.getSize() }, 1);
-    }
+  getSize() {
+    return this.children.reduce((acc, child) => acc + child.getSize(), 1);
+  }
 }
 
+const evenTree = (input) => {
+  // create a tree and reference the root
+  const rootNode = new Node('1', []);
+  const tree = parseInputAndGenerateTree(input, rootNode);
+  let stack = [tree];
+  let count = 0;
+  // dfs through the tree, assign a point value to each node
+  // every node with an even count can be cut from its parent
+  while (stack.length > 0) {
+    const current = stack.pop();
+    stack = stack.concat(current.children);
+    // we can break the conditional into a hasSubtree() function on Node
+    if (current.getSize() % 2 === 0) {
+      count += 1;
+    }
+  }
+  // reduce the count by one for the rootNode, which is always even
+  return count - 1;
+};
+
 const fileTools = require('./file.js');
-const input = fileTools.readTextFile('./even-tree-testcases/input/input00.txt')
-evenTree(input)
+
+const input = fileTools.readTextFile('./even-tree-testcases/input/input00.txt');
+console.log(evenTree(input));
