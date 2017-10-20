8e60d2f366866ad4e5439c51e6c457bf66f8292e
John Shen <johnzhihaoshen@gmail.com>
2017-10-20 15:53:38 -0700

    O(n) solution for count of smaller num

--- js/binary_search.js
+++ js/binary_search.js
@@ -1,25 +1,57 @@
 // implement binary search
-const bsearch = (arr, target) => {
-    let workingArr = arr.sort((a, b) => a - b)
-    let left = 0;
-    let right = arr.length - 1;
+// returns the index of the target
+const bsearch = (arr, target, isSorted) => {
+  let workingArr = arr;
+  if (!isSorted) {
+    workingArr = arr.sort((a, b) => a - b);
+  }
+  let left = 0;
+  let right = arr.length - 1;
 
-    while (left < right) {
-        let pivot = Math.floor((left + right) / 2)
-        if (target === workingArr[pivot]) {
-            return pivot
-        } else if (target > workingArr[pivot]) {
-            left = pivot + 1
-        } else if (target < workingArr[pivot]) {
-            right = pivot - 1
-        }
+  while (left < right) {
+    const pivot = Math.floor((left + right) / 2);
+    if (target === workingArr[pivot]) {
+      return pivot;
+    } else if (target > workingArr[pivot]) {
+      left = pivot + 1;
+    } else if (target < workingArr[pivot]) {
+      right = pivot - 1;
     }
-    // target not found
-    return null
-}
+  }
+  // target not found
+  return null;
+};
 
-// console.log(bsearch([4,12,6,3,55,2], 12))
 
-module.exports = {
-    bsearch: bsearch
-}
+const bsearchFuzzy = (sorted, target) => {
+  let left = 0;
+  let right = sorted.length - 1;
+
+  if (target <= sorted[left] || sorted.length === 0) {
+    return 0;
+  } else if (target > sorted[right]) {
+    return sorted.length;
+  }
+
+  while (left < right) {
+    const pivot = Math.floor((left + right) / 2);
+    if (sorted[pivot] < target) {
+      left = pivot + 1;
+    } else {
+      right = pivot;
+    }
+  }
+
+  return left >= target ? left : right;
+};
+
+// console.log(bsearch([4,12,6,3,55,2], 12));
+// console.log(bsearch([4,12,6,3,55,2].sort((a,b) => a - b), 12, true));
+// console.log(bsearchFuzzy([1, 3, 5, 6, 12, 36], 10)); // 4
+// console.log(bsearchFuzzy([1, 3, 5, 6, 12, 14, 36], 13)); // 5
+// console.log(bsearchFuzzy([1, 3, 5, 6, 12, 14, 36], 40)); // 7
+// console.log(bsearchFuzzy([1, 3, 5, 6, 12, 14, 36], 0)); // 0
+// console.log(bsearchFuzzy([], 9999)); // 0
+// console.log(bsearchFuzzy([-693,-369,1382,6238,7946,9774], 3439)); // 3
+
+module.exports = { bsearch, bsearchFuzzy };
--- js/count_of_smaller_num.js
+++ js/count_of_smaller_num.js
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/count-of-smaller-numbers-after-self/description/
 
+const bsearchFuzzy = require('./binary_search.js').bsearchFuzzy;
+
 const countRight = (nums, idx) => {
   let count = 0;
   const current = nums[idx];
@@ -26,9 +28,23 @@ const countSmallerBF = (nums) => {
   return counts;
 };
 
+const countSmaller = (nums) => {
+  const counts = [];
+  const sorted = [];
+  // start from the back
+  for (let i = nums.length - 1; i >= 0; i--) {
+    const current = nums[i];
+    const index = bsearchFuzzy(sorted, current, true);
+    // .splice mutates the array;
+    sorted.splice(index, 0, current);
+    counts.push(index);
+  }
+  // pushing and reversing is faster than unshift
+  return counts.reverse();
+};
+
 
 // console.log(countRight([5, 2, 6, 1], 0));
-
 console.time('countSmaller')
 console.log(countSmaller([5, 2, 6, 1]));
 console.timeEnd('countSmaller')
