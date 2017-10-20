2873f827163b807372d39f78d0be64a224784f81
John Shen <johnzhihaoshen@gmail.com>
2017-10-20 00:28:31 -0700

    added more performant sieve of erastothenes implementation for count_primes

--- js/count_primes.js
+++ js/count_primes.js
@@ -15,7 +15,7 @@ const isPrimeModulus = (num, primesLessThanNum) => {
  * @param {number} n
  * @return {number}
  */
-const countPrimes = (n) => {
+const countPrimesModulus = (n) => {
   if (n < 2) {
     return 0;
   }
@@ -29,11 +29,51 @@ const countPrimes = (n) => {
   return primes.length;
 };
 
+const isPrime = (n) => {
+  if (n <= 1) {
+    return false;
+  }
+
+  // optimization, only calculate factors up to sqrt(n);
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// sieve of erastothenes implementation with optimizations
+const countPrimes = (n) => {
+  if (n < 3) {
+    return 0;
+  }
+  const primes = Array(n).fill(true);
+  let count = n - 2;
+
+  for (let i = 2; i < n; i++) {
+    if (!primes[i]) {
+      // for some reason `count = count - 1` is almost 40% faster than `count -= 1`
+      count = count - 1;
+      continue;
+    }
+    // i is prime, set all higher multiples of i to false
+    if (isPrime(i)) {
+      // likewise, `j = j + i` is much faster than `j += i`
+      for (let j = i * i; j < n; j = j + i) {
+        primes[j] = false;
+      }
+    }
+  }
+
+  // reduce is ~30% faster than filter
+  return count;
+};
+
 
 console.log(countPrimes(2));
 console.log(countPrimes(3));
-console.log(countPrimes(4));
+console.log(countPrimes(5));
 console.time('countPrimes');
-// console.log(countPrimes(1000));
-console.log(countPrimes(499979));
+console.log(countPrimes(1500000));
 console.timeEnd('countPrimes');
