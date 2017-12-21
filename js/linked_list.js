67068f91ac3809b5b01a331904dc114916d18ad8
John Shen <johnzhihaoshen@gmail.com>
2017-12-21 12:58:36 -0800

    fix linter errors in linked list

--- js/linked_list.js
+++ js/linked_list.js
@@ -1,171 +1,168 @@
 export class ListNode {
-    constructor(value, prev=null, next=null) {
-        this.value = value;
-        this.prev = prev;
-        this.next = next;
-    }
+  constructor(value, prev = null, next = null) {
+    this.value = value;
+    this.prev = prev;
+    this.next = next;
+  }
 }
 
 export class LinkedList {
-    constructor(itemArray) {
-        // copy the array so that we don't transform the input itemArray
-        let items = itemArray.slice();
-        this.head = new ListNode(items.shift());
-        items.map(item => this.appendValue(item));
+  constructor(itemArray) {
+    // copy the array so that we don't transform the input itemArray
+    const items = itemArray.slice();
+    this.head = new ListNode(items.shift());
+    items.map(item => this.appendValue(item));
+  }
+
+  appendValue(value) {
+    const newTail = new ListNode(value, null, null);
+    this.append(newTail);
+  }
+
+  append(node) {
+    let currentNode = this.head;
+    let prev;
+    // set prev to point to the tail
+    while (currentNode !== null) {
+      prev = currentNode;
+      currentNode = currentNode.next;
+    }
+    node.prev = prev;
+    prev.next = node;
+    node.next = null;
+  }
+
+  prepend(node) {
+    if (node === this.head) {
+      throw new Error(`Cannot call prepend on the head`);
+    }
+    const oldHead = this.head;
+    oldHead.prev = node;
+    node.prev = null;
+    node.next = oldHead;
+    this.head = node;
+  }
+
+  getValues() {
+    let currentNode = this.head;
+    let values = [];
+    while (currentNode !== null) {
+      values.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return values;
+  }
+
+  delete(node) {
+    // edge case, node == head
+    if (node.prev == null) {
+      this.head = node.next
+      this.head.prev = null;
+    } else {
+      node.prev.next = node.next;
     }
 
-    appendValue(value) {
-        let newTail = new ListNode(value, null, null);
-        this.append(newTail)
+    // edge case, node == tail
+    if (node.next !== null) {
+      node.next.prev = node.prev;
     }
+  }
 
-    append(node) {
-        let currentNode = this.head;
-        let prev;
-        // set prev to point to the tail
-        while (currentNode !== null) {
-            prev = currentNode;
-            currentNode = currentNode.next;
-        }
-        node.prev = prev;
-        prev.next = node;
-        node.next = null;
+  insert(node, left, right) {
+    if (left === null || right === null) {
+      throw new Error('Cannot call insert() with null neighbors')
     }
+    left.next = node;
+    node.next = right;
+    node.prev = left;
+    right.prev = node;
+  }
 
-    prepend(node) {
-        if (node === this.head) {
-            throw new Error(`Cannot call prepend on the head`);
-        }
-        let oldHead = this.head;
-        oldHead.prev = node;
-        node.prev = null;
-        node.next = oldHead;
-        this.head = node;
+  insertAt(node, position) {
+    let currentNode = this.head;
+    let prev;
+    for (let i = 0; i < position; i++) {
+      prev = currentNode;
+      currentNode = currentNode.next;
     }
+    this.insert(node, prev, currentNode);
+  }
 
-    getValues() {
-        let currentNode = this.head;
-        let values = []
-        while (currentNode !== null) {
-            values.push(currentNode.value);
-            currentNode = currentNode.next;
-        }
-        return values;
+  dedup() {
+    const seenValues = new Set([]);
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (seenValues.has(currentNode.value)) {
+        this.delete(currentNode);
+      } else {
+        seenValues.add(currentNode.value);
+      }
+      currentNode = currentNode.next;
     }
+  }
 
-    delete(node) {
-        // edge case, node == head
-        if (node.prev == null) {
-            this.head = node.next
-            this.head.prev = null;
-        } else {
-            node.prev.next = node.next;
-        }
-
-        // edge case, node == tail
-        if (node.next !== null) {
-            node.next.prev = node.prev;
-        }
+  // Return the index of the first node with the given value
+  findIndex(value) {
+    let currentNode = this.head;
+    let currentIndex = 0;
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        return currentIndex;
+      }
+      currentNode = currentNode.next;
+      currentIndex += 1;
     }
+    throw new Error(`No node with value ${value} was found`)
+  }
 
-    insert(node, left, right) {
-        if (left === null || right === null) {
-            throw new Error('Cannot call insert() with null neighbors')
-        }
-        left.next = node;
-        node.next = right;
-        node.prev = left;
-        right.prev = node;
+  partition(value) {
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      const nextNode = currentNode.next;
+      if (currentNode.value < value) {
+        // prepend() is constant time whereas append() is linear
+        this.delete(currentNode);
+        this.prepend(currentNode);
+      }
+      currentNode = nextNode;
     }
+  }
 
-    insertAt(node, position) {
-        let currentNode = this.head;
-        let prev;
-        for (var i = 0; i < position; i++) {
-            prev = currentNode
-            currentNode = currentNode.next;
-        }
-        this.insert(node, prev, currentNode);
+  isPalindrome() {
+    const values = this.getValues();
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.value !== values.pop()) {
+        return false;
+      }
+      currentNode = currentNode.next;
     }
+    return true;
+  }
 
-    dedup() {
-        let seenValues = new Set([]);
-        let currentNode = this.head;
-        while (currentNode !== null) {
-            if (seenValues.has(currentNode.value)) {
-                this.delete(currentNode);
-            } else {
-                seenValues.add(currentNode.value);
-            }
-            currentNode = currentNode.next;
-        }
+  // If the list has an internal loop, returns the node at the beginning of the loop
+  // Returns null otherwise
+  getLoopNode() {
+    const seenNodes = new Set();
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (seenNodes.has(currentNode)) {
+        return currentNode;
+      }
+      seenNodes.add(currentNode);
+      currentNode = currentNode.next;
     }
-
-    // Return the index of the first node with the given value
-    findIndex(value) {
-        let currentNode = this.head;
-        let currentIndex = 0;
-        while (currentNode !== null) {
-            if (currentNode.value === value) {
-                return currentIndex
-            }
-            currentNode = currentNode.next
-        }
-        throw new Error(`No node with value ${value} was found`)
-    }
-
-    partition(value) {
-        let currentNode = this.head;
-        while (currentNode !== null) {
-            let nextNode = currentNode.next;
-            if (currentNode.value < value) {
-                // prepend() is constant time whereas append() is linear
-                this.delete(currentNode)
-                this.prepend(currentNode);
-            }
-            currentNode = nextNode;
-        }
-    }
-
-    isPalindrome() {
-        let values = this.getValues();
-        let currentNode = this.head;
-        while (currentNode !== null) {
-            if (currentNode.value !== values.pop()) {
-                return false;
-            }
-            currentNode = currentNode.next;
-        }
-        return true;
-    }
-
-    // If the list has an internal loop, returns the node at the beginning of the loop
-    // Returns null otherwise
-    getLoopNode() {
-        let seenNodes = new Set();
-        let currentNode = this.head;
-        while (currentNode !== null) {
-            if (seenNodes.has(currentNode)) {
-                return currentNode;
-            }
-            seenNodes.add(currentNode);
-            currentNode = currentNode.next;
-        }
-        return null;
-    }
+    return null;
+  }
 }
+
+const listToInt = list => parseInt(list.getValues().reverse().join(''), 10);
 
 // Sum two numbers represented by linked lists, where each node is a digit.
 // eg, 617 + 295 = 912 is represented by
 // (7 -> 1 -> 6) + (5 -> 9 -> 2) = (2 -> 1 -> 9)
 export const sumLists = (list1, list2) => {
-    const sum = listToInt(list1) + listToInt(list2);
-    const sumArray = sum.toString(10).split('').map(Number);
-    return new LinkedList(sumArray);
-}
-
-const listToInt = (list) => {
-    return parseInt(list.getValues().reverse().join(''));
-}
-
-
+  const sum = listToInt(list1) + listToInt(list2);
+  const sumArray = sum.toString(10).split('').map(Number);
+  return new LinkedList(sumArray);
+};
