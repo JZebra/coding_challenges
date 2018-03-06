2139c5488adc6ef2f8a5d5afa7431a0d3ccb3a93
John Shen <johnzhihaoshen@gmail.com>
2018-03-06 15:34:32 -0800

    added n_queens solution

---
+++ js/n_queens.js
@@ -0,0 +1,108 @@
+// https://leetcode.com/problems/n-queens/description/
+
+const BLANK = '#';
+const ATTACKED = '.';
+const QUEEN = 'Q';
+
+// helper function so that we don't overwrite queens
+const markAttack = (board, y, x) => {
+  board[y][x] = board[y][x] === QUEEN ? QUEEN : ATTACKED;
+};
+
+const markQueen = (board, ypos, xpos) => {
+  const boardCopy = [];
+  for (let i = 0; i < board.length; i++) {
+    boardCopy[i] = board[i].slice();
+  }
+
+  if (boardCopy[ypos][xpos] !== BLANK) {
+    throw Error('tried to set a queen on an attacked tile');
+  }
+  boardCopy[ypos][xpos] = QUEEN;
+  // start at current y because previous calls to this function will have marked
+  // rows above current row
+
+  for (let y = ypos; y < boardCopy.length; y++) {
+    for (let x = 0; x < boardCopy[0].length; x++) {
+      // check row
+      if ((x === xpos)
+      // check col
+      || (y === ypos)
+      // check NW, SE diags
+      || (y - x === ypos - xpos)
+      // check NE, SW diags
+      || (y - ypos === xpos - x)) {
+        markAttack(boardCopy, y, x);
+      }
+    }
+  }
+  return boardCopy;
+};
+
+const createBoard = (n) => {
+  const board = [];
+  for (let i = 0; i < n; i++) {
+    board.push(Array(n).fill(BLANK));
+  }
+  return board;
+};
+
+const countSquares = (board, squareType) => {
+  return board.reduce((acc, row) => {
+    return acc + row.reduce((acc2, cell) => {
+      return cell === squareType ? acc2 + 1 : acc2;
+    }, 0);
+  }, 0)
+};
+
+const solve = (board) => {
+  const openSpaceCount = countSquares(board, BLANK);
+  const queenCount = countSquares(board, QUEEN);
+  const size = board.length;
+  if (openSpaceCount === 0 && queenCount === size) {
+    return board;
+  }
+
+  // optimization, we can have max 1 queen per row, so start at current queencount
+  for (let y = queenCount; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      if (board[y][x] === BLANK) {
+        solution = solve(markQueen(board, y, x))
+        if (solution) {
+          return solution;
+        }
+      }
+    }
+  }
+}
+
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+const solveNQueens = (n) => {
+  // each row must have a queen
+  // init board
+  // place initial queen and mark attacked positions
+  // continue placing queens
+  // if we have no possible placements, backtrack
+  // store solutions
+  if (n === 0 || n === 2 || n === 3) {
+    return [];
+  }
+
+  const solutions = [];
+  // each queen position on the first row will have at most one solution
+  for (let i = 0; i < n; i++) {
+    let board = createBoard(n);
+    board = markQueen(board, 0, i);
+
+    // try to solve
+    const solution = solve(board);
+    if (solution) {
+      solutions.push(solution);
+    }
+  }
+
+  return solutions;
+};
